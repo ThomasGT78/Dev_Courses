a33e8a2fe8520ff43d0280daa32f91751159ffc1
thomas <thomas.garot@gmail.com>
2023-10-21 18:40:41 +0200

    Add courses: Questionnaires, Tools Dev

---
+++ Tools Dev - IDE/Visual Studio Code/VSC Shortcuts.js
@@ -0,0 +1,22 @@
+// Open shortcut SearchBar
+ctrl + shift + P
+/* 
+You modify directly a part of the text:
+Select the text
+ctrl + shift + P
+type the action ("uppercase" for exemple)
+Enter "Transform to Uppercase."
+*/
+
+
+// Uppercase
+ctrl + u	ctrl + u
+
+// Lowercase
+ctrl + l	ctrl + l
+
+// Title Case (first letter of each word)
+ctrl + t	ctrl + t
+
+// Toggle Case
+???
